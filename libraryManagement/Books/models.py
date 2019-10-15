2ebd2de024885405165aa04383e62605f8c267dd
Amit Tiwari <amit@addictionbazaar.com.com>
2019-10-16 00:09:36 +0530

    module created

--- libraryManagement/Books/models.py
+++ libraryManagement/Books/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+	ISBN = models.CharField(max_length=10)
+	title = models.CharField(max_length=250)
+	Class = models.CharField(max_length=20)
+	grade = models.CharField(max_length=20)
+	semester = models.CharField(max_length=10)
--- libraryManagement/Student/models.py
+++ libraryManagement/Student/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+	name = models.CharField(max_length=35)
+	course = models.CharField(max_length=20)
+	semester = models.CharField(max_length=10);
+	email = models.CharField(max_length=10)
+	mobile = models.CharField(max_length=10)
