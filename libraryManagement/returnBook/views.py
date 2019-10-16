4bcca1290411f4aba11cd06537fd109ac2e54114
Amit Tiwari <amit@addictionbazaar.com.com>
2019-10-16 17:09:36 +0530

    update

--- libraryManagement/returnBook/views.py
+++ libraryManagement/returnBook/views.py
@@ -1,27 +1,30 @@
 from django.shortcuts import render
 # from LendBook.models import LendBook
-# from Books.models import Book
+from Books.models import Book
 from django.http import HttpResponseRedirect
 from django.urls import reverse
-# from Student.models import Student
+from Student.models import Student
 from .forms import ReturnBookForm
 # Create your views here.
 
 
 def returnBook(request):
     # obj_lend = LendBook.objects.all()
-    # obj_book = Book.objects.all()
-    # obj_student = Student.objects.all()
+    obj_book = Book.objects.last()
+    obj_student = Student.objects.last()
     error = ''
     if request.method == "POST":
-
-        return_book = ReturnBookForm(request.POST)
+        return_book = ReturnBookForm(request.POST or None)
         context = {
             'return_book': return_book,
             'errors': error
         }
         if return_book.is_valid():
-            return_book.save()
+            data = ReturnBookForm.cleaned_data.get(return_book.ISBN)
+            obj_book = Book.objects.filter(ISBN=data)
+            data1 = ReturnBookForm.cleaned_data.get(return_book.UID)
+            obj_student = Student.objects.filter(UID=data1)
+            # return_book.save()
             return HttpResponseRedirect(reverse('index'))
     else:
         return_book = ReturnBookForm()
@@ -29,5 +32,9 @@ def returnBook(request):
             'return_book': return_book,
             'errors': error
         }
-    return render(request, "return-book.html",
-                  context)
+    return render(request, "return-book.html", {
+        'obj_book': obj_book,
+        'obj_student': obj_student
+    },
+
+        context)
